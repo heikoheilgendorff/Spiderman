60dc909873c2ce7fcc28e85298ea3f629cb48067
heikoheilgendorff <heikoheilgendorff@users.noreply.github.com>
2017-10-25 16:43:39 +0200

    first commit

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -9,7 +9,7 @@ class QuotesSpider(scrapy.Spider):
             'http://quotes.toscrape.com/page/2/',
         ]
         for url in urls:
-            yield scrapy.Request(url-url, callback=self.parse)
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         page = response.url.split("/")[-2]
